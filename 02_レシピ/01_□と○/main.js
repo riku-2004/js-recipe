46bbb11afa0ab48cb46017e66d733bb1d369f8d2
riku-2004 <lubii2004@gmail.com>
2024-11-17 18:56:33 +0900

    aaa

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,8 @@
 const figure = document.getElementById("figure")
 
-figure.onclick = function () {
+figure.onmouseover = function () {
+  figure.classList.toggle("rounded")
+}
+figure.onmouseleave = function () {
   figure.classList.toggle("rounded")
 }
